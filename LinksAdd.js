ef420d47807178d1119ae8458793f659490ea8d8
tarvinn <31428767+tarvinn@users.noreply.github.com>
2017-09-05 19:39:13 -0400

    Add files via upload

---
+++ LinksAdd.js
@@ -0,0 +1,217 @@
+﻿
+//'Create Users Manager
+
+//iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\1. Admin Log In");
+	iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\2. Admin Area Navigation");
+		iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\3. User Management Navigation");
+			iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\4. User Management Wizard Navigation");	
+				iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\5. User Management Wizard Add User Button");
+			
+//'Above is the steps used to navigate to the wizard.
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\6. Create User Data Manager");
+	iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\7. User Wiz Next Button");
+		iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\8. User Wiz Next Button");
+		
+//'From this point you decide if you wish to create more users.
+
+//'Add Another User
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\9. Add Another User Button");
+
+//'Create Users Add
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\10. Create User Data Add");
+	iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\11. User Wiz Next Button");
+		iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\12. User Wiz Next Button");
+
+//Create User Finish Button
+
+iimPlay  ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\13. Create User Finish Button");
+
+//Create User User ID Signature
+
+iimPlay  ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\14. Create User Signature User ID");
+
+//Create User User Password Signature
+
+iimPlay  ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\15. Create User Signature User Password");
+
+//Sign And Finish
+
+iimPlay  ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\16. User Wizard Sign And Finish Button");
+
+//Sign And Finish
+
+iimPlay  ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Users\\17. User Wizard Exit Button");
+
+
+
+
+//'Create Project Center Functional Area
+
+
+
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\18. Admin Area Navigation");
+	iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\19. Functional Area Wizard Navigation");
+		iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\20. Add A Functional Area");
+
+
+//'Select Functional Area Radio Button
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\21. Project Center Radio Button");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\22. Next Button");
+
+//'Functional Area Name
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\23. Project Center Name");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\24. Next Button");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\25. Next Button");
+
+//'Here is where you will choose what modules to add.
+
+//'Add Links Center And General
+
+	iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\26. Links Center");
+	iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\27. Links General");
+	
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\28. Next Button");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\29. Next Button");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\30. Next Button");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\31. Next Button");
+
+//'Here is where you Choose to add Groups or users as Participants
+
+	//'Add Participant
+
+	iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\32. Participant Add");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\33. Next Button");
+
+	//'Add Manager
+	
+	iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\34. Manager Add");
+	
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\35. Next Button");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Project Center\\36. Finish Button");
+
+
+
+
+//'Create Subject Center Functional Area
+
+
+
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\37. Admin Area Navigation");
+	iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\38. Functional Area Wizard Navigation");
+		iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\39. Add A Functional Area");
+
+
+//'Select Functional Area Radio Button
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\40. Subject Center Radio Button");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\41. Next Button");
+
+//'Functional Area Name
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\42. Subject Center Name");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\43. Next Button");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\44. Next Button");
+
+//'Here is where you will choose what modules to add.
+
+//'Add Links Center And General
+
+	iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\45. Links Center");
+	iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\46. Links General");
+	
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\47. Next Button");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\48. Next Button");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\49. Next Button");
+
+
+//'Here is where you Choose to add Groups or users as Participants
+
+//'Add Participant
+
+	iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\51. Participant Add");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\52. Next Button");
+
+//'Add Manager
+	
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\53. Manager Add");
+	
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\54. Next Button");
+
+//'Next
+
+iimPlay ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Create Subject Center\\55. Finish Button");
+
+
+
+
+//'Update Module Roles
+
+
+
+
+iimPlay  ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Module Roles\\56. Add");
+
+
+
+
+//'Test Execution
+
+
+iimPlay  ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Set Up\\57. Create Projects and Assign Participants and Roles");
+iimPlay  ("Infostrength 6.0\\Test Case Scenario\\Links (Center)\\Links (Center)-Add\\Test Macros\\Test Execution\\Links-Add");
